2a971c229f7881f88abf0b54ad3f8ad72daa039e
John Baldwin <johnbaldwin82@gmail.com>
2023-03-28 13:56:31 -0600

    tutor session 2

--- end.js
+++ end.js
@@ -1,34 +1,47 @@
 const username = document.querySelector('#username')
 const saveScoreBtn = document.querySelector('#saveScoreBtn')
 const finalScore = document.querySelector('#finalScore')
-const mostRecentScore = document.querySelector('#mostRecentScore')
+const mostRecentScore = JSON.parse(localStorage.getItem('mostRecentScore'))
 
 const highScores = JSON.parse(localStorage.getItem('highscores')) || []
 
-const MAX_HIGH_SCORES = $
+// const MAX_HIGH_SCORES = 5
 
-finalScore.innerText = mostRecentScore
+//finalScore.textContent = 
 
-username.addEventListener('keyup', () => {
-    saveScoreBtn.disabled = !username.value
-})
 
-saveHighScore = e => {
-    e.preventDefault()
-
-    const score = {
+function captureScore() {
+    console.log(mostRecentScore)
+    const newPerson = {
         score: mostRecentScore,
         name: username.value
     }
 
-    highScores.push(score)
-
-    highScores.sort((a,b) =>{
-    return b.score - a.score
-    })
+    highScores.push(newPerson)
+    localStorage.setItem('highscores', JSON.stringify (highScores))
+    window.location.assign('/')
 }
+saveScoreBtn.addEventListener('click', captureScore)
+// username.addEventListener('keyup', () => {
+//     saveScoreBtn.disabled = !username.value
+// })
 
-highScores.splice[5]
+// saveHighScore = e => {
+//     e.preventDefault()
 
-localStorage.setItem('highScores', JSON.stringify(highScores))
-window.location.assign('/')
+//     const score = {
+//         score: mostRecentScore,
+//         name: username.value
+//     }
+
+//     highScores.push(score)
+
+//     highScores.sort((a,b) =>{
+//     return b.score - a.score
+//     })
+// }
+
+// highScores.splice[5]
+
+// localStorage.setItem('highScores', JSON.stringify(highScores))
+// window.location.assign('/')
