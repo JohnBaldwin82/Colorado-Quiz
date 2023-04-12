e3d74c2ef631db97b769ae05bc7b81a79821a317
John Baldwin <johnbaldwin82@gmail.com>
2023-04-12 12:15:54 -0600

    after tutor session

--- end.js
+++ end.js
@@ -2,7 +2,7 @@ const username = document.querySelector('#username')
 const saveScoreBtn = document.querySelector('#saveScoreBtn')
 const finalScore = document.querySelector('#final-score')
 const mostRecentScore = JSON.parse(localStorage.getItem('mostRecentScore'))
-
+const nameScore = document.querySelector('#nameScore')
 const highScores = JSON.parse(localStorage.getItem('highScores')) || []
 
 // const MAX_HIGH_SCORES = 5
@@ -11,6 +11,7 @@ finalScore.innerHTML = mostRecentScore
 
 
 function captureScore() {
+    event.preventDefault()
     console.log(mostRecentScore)
     const newPerson = {
         score: mostRecentScore,
@@ -19,8 +20,19 @@ function captureScore() {
 
     highScores.push(newPerson)
     localStorage.setItem('highScores', JSON.stringify (highScores))
-    window.location.assign('/')
+    displayScore()
+    // window.location.assign('/')
 }
+
+function displayScore() {
+    var highScoresArray = JSON.parse(localStorage.getItem('highScores'))
+    for (let index = 0; index < highScoresArray.length; index++) {
+        var personScore = document.createElement('p')
+        personScore.innerHTML = `${highScoresArray[index].name}: ${highScoresArray[index].score}`
+        nameScore.appendChild(personScore)
+    }
+}
+
 saveScoreBtn.addEventListener('click', captureScore)
 
 // username.addEventListener('keyup', () => {
